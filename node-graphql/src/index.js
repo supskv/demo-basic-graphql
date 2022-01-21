6e31d4c8f2de268a1f25a70b1225cadd388f5b75
suphawich <suphawich.sungkhavorn@capco.com>
2022-01-21 14:54:33 +0700

    basic graphql with nodejs

---
+++ node-graphql/src/index.js
@@ -0,0 +1,52 @@
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
+
+// 2
+let links = [{
+  id: 'link-0',
+  url: 'www.google.com',
+  description: 'GraphQL'
+}]
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args, context) => {
+      let idCount = links.length
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+  },
+  // 3
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
+}
+
+// 3
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
